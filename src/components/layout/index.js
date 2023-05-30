537c4a01aa26d2207c87f1349be531f53d7cd29a
febriqgal <febriqgalp@gmail.com>
2023-05-30 19:31:02 +0700

    dasdasdasdasdasd

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,47 +1,169 @@
+/* eslint-disable @next/next/no-img-element */
+import { Fragment } from "react";
+import { Disclosure, Menu, Transition } from "@headlessui/react";
+import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
-import React from "react";
-import { Poppins } from "next/font/google";
 import { useRouter } from "next/router";
-const poppins = Poppins({
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  subsets: ["devanagari", "latin", "latin-ext"],
-});
+import Logo from "../../../public/logo.jpg";
+import Image from "next/image";
 const navigation = [
-  { title: "Beranda", href: "/" },
-  { title: "Jadwal Dokter", href: "/jadwal-dokter" },
-  { title: "Sejarah", href: "/sejarah" },
-  { title: "Struktur Organisasi", href: "/struktur-organisasi" },
+  { name: "Beranda", href: "/" },
+  { name: "Jadwal Dokter", href: "/jadwal-dokter" },
+  { name: "Sejarah", href: "/sejarah" },
+  { name: "Struktur Organisasi", href: "/struktur-organisasi" },
 ];
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
 export default function Layout({ children }) {
   const route = useRouter();
   return (
-    <div className={poppins.className}>
-      <div className="flex justify-between items-center mx-20">
-        <h1 className="text-lg font-bold">Puskesmas Kambang</h1>
-        <nav className="flex justify-center items-center gap-4 py-4">
-          {navigation.map((e, i) => {
-            return (
-              <Link
-                className={
-                  route.pathname != e.href ? "" : "underline underline-offset-4"
-                }
-                key={i}
-                href={e.href}
-              >
-                {e.title}
-              </Link>
-            );
-          })}
-          <Link
-            className="bg-green-700 text-slate-50 rounded-lg px-4"
-            href={"/login"}
-          >
-            Login
-          </Link>
-        </nav>
-      </div>
-      <main>{children}</main>
-      <footer className="text-center my-4">2023 - Puskesmas Kambang</footer>
-    </div>
+    <Disclosure as="nav">
+      {({ open }) => (
+        <>
+          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 ">
+            <div className="relative flex h-16 items-center justify-between">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                {/* Mobile menu button*/}
+                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-slate-950 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                <div className="flex flex-shrink-0 items-center">
+                  <Image
+                    className="block h-8 w-auto lg:hidden"
+                    src={Logo}
+                    alt="Your Company"
+                  />
+                  <Image
+                    className="hidden h-8 w-auto lg:block"
+                    src={Logo}
+                    alt="Your Company"
+                  />
+                </div>
+                <div className="hidden sm:ml-6 sm:block">
+                  <div className="flex space-x-4">
+                    {navigation.map((item) => (
+                      <Link
+                        key={item.name}
+                        href={item.href}
+                        className={
+                          route.pathname != item.href
+                            ? "text-slate-950 hover:bg-slate-950 duration-700 transition-all hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                            : "bg-slate-950 text-white rounded-md px-3 py-2 text-sm font-medium"
+                        }
+                        aria-current={item.current ? "page" : undefined}
+                      >
+                        {item.name}
+                      </Link>
+                    ))}
+                  </div>
+                </div>
+              </div>
+              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                {/* Profile dropdown */}
+                <Menu as="div" className="relative ml-3">
+                  <div>
+                    <Menu.Button className="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                      <span className="sr-only">Open user menu</span>
+                      <img
+                        className="h-8 w-8 rounded-full"
+                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                        alt=""
+                      />
+                    </Menu.Button>
+                  </div>
+                  <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-100"
+                    enterFrom="transform opacity-0 scale-95"
+                    enterTo="transform opacity-100 scale-100"
+                    leave="transition ease-in duration-75"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95"
+                  >
+                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                      <Menu.Item>
+                        <h1 className={"font-bold block px-4 py-2 text-sm"}>
+                          Febriqgal Purnama
+                        </h1>
+                      </Menu.Item>
+
+                      <Menu.Item>
+                        {({ active }) => (
+                          <Link
+                            href="#"
+                            className={classNames(
+                              active ? "bg-gray-100" : "",
+                              "block px-4 py-2 text-sm text-gray-700"
+                            )}
+                          >
+                            Dashboard Admin
+                          </Link>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <Link
+                            href="#"
+                            className={classNames(
+                              active ? "bg-gray-100" : "",
+                              "block px-4 py-2 text-sm text-gray-700"
+                            )}
+                          >
+                            Pengaturan
+                          </Link>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <Link
+                            href="#"
+                            className={classNames(
+                              active ? "bg-gray-100" : "",
+                              "block px-4 py-2 text-sm text-gray-700"
+                            )}
+                          >
+                            Keluar
+                          </Link>
+                        )}
+                      </Menu.Item>
+                    </Menu.Items>
+                  </Transition>
+                </Menu>
+              </div>
+            </div>
+          </div>
+          <Disclosure.Panel className="sm:hidden">
+            <div className="space-y-1 px-2 pb-3 pt-2 flex flex-col">
+              {navigation.map((item) => (
+                <Link
+                  key={item.name}
+                  href={item.href}
+                  className={
+                    route.pathname != item.href
+                      ? "text-slate-950 hover:bg-slate-950 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                      : "bg-slate-950 text-white rounded-md px-3 py-2 text-sm font-medium"
+                  }
+                  aria-current={item.current ? "page" : undefined}
+                >
+                  {item.name}
+                </Link>
+              ))}
+            </div>
+          </Disclosure.Panel>
+          <main>{children}</main>
+          <footer className="text-center py-4">2023 - Puskesmas Kambang</footer>
+        </>
+      )}
+    </Disclosure>
   );
 }
